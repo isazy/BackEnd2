e18fdb16d9c8073fb8594b1d293a5dd898c9106a
isazyGitHub <isabelyf270@gmail.com>
2023-11-09 13:43:27 -0300

    back-end

---
+++ src/main/java/com/projetos/repository/CursosRepository.java
@@ -0,0 +1,9 @@
+package com.projetos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetos.entities.Cursos;
+
+public interface CursosRepository  extends JpaRepository<Cursos, Long>{
+
+}
